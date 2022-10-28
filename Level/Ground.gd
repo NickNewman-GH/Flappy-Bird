6bd205bf733b9009df006fe2e32bdb7c432aac33
Nikita <nitrix38r@gmail.com>
2022-10-28 14:28:56 +0800

    resize and ground moving

--- Level/Ground.gd
+++ Level/Ground.gd
@@ -3,20 +3,29 @@ extends StaticBody2D
 var velocity = Vector2(-1, 0)
 var speed = 150
 var screen_size
+var size
 var travelled_distance = 0
 
-# Called when the node enters the scene tree for the first time.
+func reset_position():
+	position.x = size.x * 5
+
 func _ready():
 	screen_size = get_viewport().size
-	position.y = screen_size.y
+	print($CollisionShape2D.shape.extents)
+	size = $CollisionShape2D.shape.extents
+	#position.y = screen_size.y
+	#reset_position()
 
 func _process(delta):
 	position += velocity * speed * delta
-	travelled_distance -= velocity.x * speed * delta
-	print(travelled_distance)
-	if travelled_distance >= 150:
-		position.x += travelled_distance
-		travelled_distance = 0
+#	travelled_distance -= velocity.x * speed * delta
+#	print(travelled_distance)
+#	if travelled_distance >= 150:
+#		position.x += travelled_distance
+#		travelled_distance = 0
 
 func stop():
 	speed = 0
+
+func _on_VisibilityNotifier2D_viewport_exited(viewport):
+	reset_position()
